e9273159092e6523886b209e9098813e03f38304
Natony <20151230@student.hcmute.edu.vn>
2023-07-18 19:41:32 +0700

    second commit

---
+++ codeProject2/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://learn.microsoft.com/vi-vn/training/modules/csharp-do-while/6-review-solution-differentiate-while-do-statements
+string? readResult;
+string roleName = "";
+bool validEntry = false;
+
+do
+{                
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        roleName = readResult.Trim();
+    }
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+readResult = Console.ReadLine();
---
+++ findCoins/Program.cs
@@ -0,0 +1,36 @@
+﻿int target = 80;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if(result.Length == 0){
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine("Change found at positions:");
+    for(int i = 0; i < result.GetLength(0); i++){
+        if(result[i, 0] == -1){
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
+
+
+int[,] TwoCoins(int[] coins,int target){
+    int[,] result = {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}};
+    int count = 0;
+    for(int i = 0; i < coins.Length; i++){
+        for(int j = i + 1; j < coins.Length; j++){
+            if(coins[i] + coins[j] == target){
+                result[count, 0] = i;
+                result[count, 1] = j;
+                count++;
+            }
+            if(count == result.GetLength(0)){
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0,0] : result;
+}
